8d37c5779d39ffbca08ead0b7f26ffdc50a299af
Raphael Scaff <raphael.scaff@vadetaxi.com.br>
2024-03-09 10:52:19 -0300

    Termina aula 4 do curso de oop-python

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,26 +1,31 @@
-
+from modelos.avaliacao import Avaliacao
 
 class Restaurante:
 
     restaurantes = []
 
     def __init__(self, nome: str, categoria: str): # selt -> Referência da instância
-        variaveis = [nome, categoria]
-        if not all(isinstance(var, str) for var in variaveis):
-            raise TypeError("Nome e categoria devem ser strings")
-        self._nome = nome.title()
-        self._categoria = categoria.upper()
+        self._nome = nome
+        self._categoria = categoria
+        self.validar_restaurante()
         self._status = False
+        self._avaliacao = []
         Restaurante.restaurantes.append(self) # Adiciona o restaurante no próprio restaurante.
 
     def __str__(self):
         return f'{self._nome} | {self._categoria}'
     
+    def validar_restaurante(self):
+        variaveis = [self._nome, self._categoria]
+        if not all(isinstance(var, str) for var in variaveis):
+            raise TypeError("Nome e categoria devem ser strings")
+    
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | {'Status'}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.status}')
+            print(f'{restaurante._nome.title().ljust(25)} | {restaurante._categoria.upper().ljust(25)} | ' +
+                  f'{str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.status}')
 
 
     @property
@@ -28,4 +33,17 @@ class Restaurante:
         return '✅' if self._status else '❌'
     
     def ativa_restaurante(self):
-        self._status = not self._status
+        self._status = not self._status 
+    
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
+
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return 0
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        qnt_notas = len(self._avaliacao)
+        media = soma_das_notas / qnt_notas
+        return media.__round__(1)
